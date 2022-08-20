2af1ba809a114f42a9f7d7db943a8bd8961c2cc5
chari00001 <90192334+chari00001@users.noreply.github.com>
2022-08-20 11:34:58 +0300

    Form and button refinements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,47 +1,64 @@
 // Initializing connection
 const socket = io()
 
+// Selecting elements from DOM
+const $messageForm = document.getElementById('form')
+const $messageFormInput = document.getElementById('message')
+const $messageFormButton = document.getElementById('send')
+const $locationSendButton = document.getElementById('send-location')
+
 // Receiving message event with message variable, then printing it to the console
 socket.on('message', (message) => {
     console.log(message);
 })
 
-// Getting form by id and adding submit event listener
-document.getElementById('form')
-    .addEventListener('submit', (e) => {
-        // Prevents page refreshing on form submission
-        e.preventDefault()
+// Adding submit event listener to form element
+$messageForm.addEventListener('submit', (e) => {
+    // Prevents page refreshing on form submission
+    e.preventDefault()
 
-        // Selecting message from input field
-        const message = e.target.elements.message.value
+    // Form button will be disabled for a short time to prevent accidental double clicks
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
-        // Emitting event to server with message value
-        socket.emit('sendMessage', message, (message) => {
-            if(error){
-                return console.log(error);
-            }
-            // Acknowledgement message
-            console.log('Message Delivered.');
-        })
-    })
+    // Selecting message from input field
+    const message = e.target.elements.message.value
 
-// Location sharing field selected here and added click event handler 
-document.getElementById('send-location')
-    .addEventListener('click', (e) => {
-        // Error handler for browsers doesn't support geolocation
-        if(!navigator.geolocation){
-            return alert('Geolocation is not supported by your browser.')
+    // Emitting event to server with message value
+    socket.emit('sendMessage', message, (message) => {
+        // Re-enabling button, emptying it and refocusing to input
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error);
         }
-
-        // If browser has support send destructured coordinates to server with sendLocation event
-        navigator.geolocation.getCurrentPosition((position) => {
-            const {latitude, longitude} = position.coords
-            socket.emit('sendLocation', {
-                latitude,
-                longitude
-            },
-            () => {
-               console.log('Location shared.'); 
-            })
-        }) 
+        // Acknowledgement message
+        console.log('Message Delivered.');
     })
+})
+
+// Adding click event listener to location button
+$locationSendButton.addEventListener('click', (e) => {
+    // Error handler for browsers doesn't support geolocation
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    // Disabling button until it sends location successfully
+    $locationSendButton.setAttribute('disabled', 'disabled')
+
+    // If browser has support send destructured coordinates to server with sendLocation event
+    navigator.geolocation.getCurrentPosition((position) => {
+        const {latitude, longitude} = position.coords
+        socket.emit('sendLocation', {
+            latitude,
+            longitude
+        },
+        () => {
+            // Re-enabling button
+            $locationSendButton.removeAttribute('disabled')
+            console.log('Location shared.'); 
+        })
+    }) 
+})
